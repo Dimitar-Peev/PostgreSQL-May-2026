805740d2191e46be827d58fd4373ce42d9f46ebc
Dimitar-Peev <mctrix87@abv.bg>
2026-06-14 13:34:16 +0300

    Solution - Task 11

---
+++ 09. Regular Exam - 13 June 2026/11. Update Loyalty Status.sql
@@ -0,0 +1,27 @@
+DROP PROCEDURE IF EXISTS udp_update_loyalty_status;
+--
+CREATE OR REPLACE PROCEDURE udp_update_loyalty_status("min_orders" INT)
+AS
+$$
+BEGIN
+    UPDATE "customers"
+    SET "loyalty_card" = TRUE
+    WHERE "id" IN (SELECT "customer_id"
+                   FROM "orders"
+                   GROUP BY "customer_id"
+                   HAVING COUNT("id") >= "min_orders");
+END;
+$$
+    LANGUAGE plpgsql;
+--
+CALL udp_update_loyalty_status(4);
+
+SELECT
+    "id",
+    "first_name",
+    "last_name",
+    "loyalty_card"
+FROM
+    "customers"
+ORDER BY
+    "id" ASC;
