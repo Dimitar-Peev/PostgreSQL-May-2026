05c363d70c3dcd03395547b85f5104f3d0e6ecfd
Dimitar-Peev <mctrix87@abv.bg>
2026-06-18 20:18:33 +0300

    Solution - Task 10

---
+++ 07. Database Programmability/02. Exercise/10. Money Transfer.sql
@@ -0,0 +1,20 @@
+DROP PROCEDURE IF EXISTS sp_transfer_money;
+--
+CREATE OR REPLACE PROCEDURE sp_transfer_money("sender_id" INT, "receiver_id" INT, "amount" NUMERIC(10, 4))
+AS
+$$
+DECLARE
+    "current_balance" NUMERIC;
+BEGIN
+    SELECT "balance"
+    INTO "current_balance"
+    FROM "accounts"
+    WHERE "id" = "sender_id";
+
+    IF "current_balance" - "amount" >= 0 THEN
+        CALL sp_withdraw_money("sender_id", "amount");
+        CALL sp_deposit_money("receiver_id", "amount");
+    END IF;
+END;
+$$
+    LANGUAGE plpgsql;
