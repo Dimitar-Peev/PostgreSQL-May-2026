42acaae6ecbe2653b69c868567899ed0952f86f9
Dimitar-Peev <mctrix87@abv.bg>
2026-07-06 12:07:10 +0300

    Solution - Task 6

---
+++ 08. Exam Preparation/02. Exercise/06. Drivers and Cars.sql
@@ -0,0 +1,13 @@
+SELECT d."first_name",
+       d."last_name",
+       c."make",
+       c."model",
+       c."mileage"
+FROM "drivers" AS d
+         INNER JOIN "cars_drivers" AS cd
+                    ON d."id" = cd."driver_id"
+         INNER JOIN cars AS c
+                    ON cd."car_id" = c."id"
+WHERE c."mileage" IS NOT NULL
+ORDER BY c."mileage" DESC,
+         d."first_name";
