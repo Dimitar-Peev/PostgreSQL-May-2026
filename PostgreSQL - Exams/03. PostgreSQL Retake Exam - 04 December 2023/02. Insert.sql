19c0275af5bf86abf9656f3d3ac3edf0de1e4265
Dimitar-Peev <mctrix87@abv.bg>
2026-07-17 20:01:14 +0300

    Solution - Task 2

---
+++ PostgreSQL - Exams/03. PostgreSQL Retake Exam - 04 December 2023/02. Insert.sql
@@ -0,0 +1,16 @@
+CREATE TABLE "gift_recipients"
+(
+    "id"         INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "name"       VARCHAR(75),
+    "country_id" INT,
+    "gift_sent"  BOOLEAN DEFAULT FALSE
+);
+
+INSERT INTO "gift_recipients" ("name", "country_id", "gift_sent")
+SELECT CONCAT("first_name", ' ', "last_name") AS "name",
+       "country_id",
+       CASE
+           WHEN "country_id" IN (7, 8, 14, 17, 26) THEN TRUE
+           ELSE FALSE
+           END                                AS "gift_sent"
+FROM "customers";
