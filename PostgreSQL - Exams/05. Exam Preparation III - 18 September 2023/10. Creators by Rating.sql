18d9fe5a634a68e54e6ef7217edf5abf49004e20
Dimitar-Peev <mctrix87@abv.bg>
2026-08-01 00:25:00 +0300

    Solution - Task 10

---
+++ PostgreSQL - Exams/05. Exam Preparation III - 18 September 2023/10. Creators by Rating.sql
@@ -0,0 +1,14 @@
+SELECT c."last_name",
+       CEIL(AVG(bg."rating")) AS "average_rating",
+       p."name"               AS "publisher_name"
+FROM "creators" AS c
+         INNER JOIN "creators_board_games" AS cbg
+                    ON c."id" = cbg."creator_id"
+         INNER JOIN "board_games" AS bg
+                    ON cbg."board_game_id" = bg."id"
+         INNER JOIN "publishers" AS p
+                    ON bg."publisher_id" = p."id"
+WHERE p."name" = 'Stonemaier Games'
+GROUP BY c."last_name",
+         p."name"
+ORDER BY "average_rating" DESC;
